27c808948e78d486ebc1a72867f1a3fa306f7c63
kristibyrnes <kristibyrnes@icloud.com>
2018-01-07 17:53:10 -0500

    Deliverable 2

---
+++ script.js
@@ -0,0 +1,63 @@
+//exceriseOne
+
+var num=prompt("Enter a number!");
+var total = 0;
+for (var num = 0; num <= 5; num++) {
+	total += num;
+}
+
+console.log (total);
+
+//exceriseTwo
+
+// var str='';
+// var play=prompt("Do you want to play?");
+//
+// if (play=="yes") {
+//   do {
+//     var word=prompt("Enter a word.");
+//     str = str + ' ';
+//     str = str + word;
+//     play=prompt("Do you want to play again?");
+//   } while(play=="yes");
+// }
+//
+// console.log(str);
+
+//exceriseThree
+//
+// var outcome = 'Hello my name is Adam';
+// var condition = prompt('Would you like to print your name?');
+//
+// if(condition == 'yes') {
+//   console.log(outcome);
+//
+//   while(condition == 'yes') {
+//     condition = prompt('Would you like to print your name again?');
+//     if(condition == 'yes') {
+//       outcome += '!';
+//       console.log(outcome);
+//     }
+//   }
+// }
+
+//exceriseFour
+//
+// var breakfast = 'Since it is morning, you should be eating breakfast. We suggest eggs and toast.'
+// var lunch = 'Since it is noon, you should be eating lunch. We suggest a salad.'
+// var dinner = 'Since it is evening, you should be eating dinner. We suggest chicken and rice.'
+//
+// var input=prompt('What time of day is it?');
+//
+// if (input == 'morning') {
+//   console.log (breakfast);
+// }
+// else if (input == 'noon') {
+//   console.log(lunch);
+// }
+// else if (input == 'evening') {
+//   console.log (dinner);
+// }
+// else {
+//   console.log('invalid input');
+// }
